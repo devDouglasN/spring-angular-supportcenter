d7cadb11ddb7407d45cf094fd4f11b8ecf067139
Douglas <douglas_timaofiel@hotmail.com>
2023-08-21 20:25:25 -0300

    Initial commit

---
+++ src/main/java/com/douglas/centralsupport/CentralsupportApplication.java
@@ -0,0 +1,13 @@
+package com.douglas.centralsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralsupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralsupportApplication.class, args);
+	}
+
+}
