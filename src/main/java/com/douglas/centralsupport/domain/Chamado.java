fa985e741f428ce7f4b86b2f8fc723232b9f0c75
Douglas <douglas_timaofiel@hotmail.com>
2023-08-22 20:47:19 -0300

    Mapeamento JPA nas Entidades de Domínio

--- src/main/java/com/douglas/centralsupport/domain/Chamado.java
+++ src/main/java/com/douglas/centralsupport/domain/Chamado.java
@@ -1,22 +1,47 @@
 package com.douglas.centralsupport.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import org.hibernate.annotations.ManyToAny;
+
 import com.douglas.centralsupport.domain.enums.Prioridade;
 import com.douglas.centralsupport.domain.enums.Status;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
-public class Chamado {
-	
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Chamado implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataAbertura;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento = LocalDate.now();
 	private Prioridade prioridade;
 	private Status status;
 	private String título;
 	private String observaçoes;
 	
+	@ManyToOne
+	@JoinColumn(name = "tecnico_id")
 	private Tecnico tecnico;
+	
+	@ManyToOne
+	@JoinColumn(name = "cliente")
 	private Cliente cliente;
 	
 	public Chamado() {
--- src/main/java/com/douglas/centralsupport/domain/Cliente.java
+++ src/main/java/com/douglas/centralsupport/domain/Cliente.java
@@ -3,12 +3,22 @@ package com.douglas.centralsupport.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.douglas.centralsupport.domain.enums.Perfil;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Cliente extends Pessoa {
 
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 	
 	public Cliente() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
