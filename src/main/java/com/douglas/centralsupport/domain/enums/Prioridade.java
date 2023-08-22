99a447f3546d6bf38134d6b149756776abd907f4
Douglas <douglas_timaofiel@hotmail.com>
2023-08-22 01:01:01 -0300

    Enums criados

---
+++ src/main/java/com/douglas/centralsupport/domain/enums/Prioridade.java
@@ -0,0 +1,39 @@
+package com.douglas.centralsupport.domain.enums;
+
+public enum Prioridade {
+	
+	BAIXA(0, "BAIXA"),
+	MÉDIA(1, "MÉDIA"),
+	ALTA(2, "ALTA");
+	
+	private Integer codigo;
+	private String descricao;
+	
+	private Prioridade(Integer codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static Prioridade toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		
+		for(Prioridade x : Prioridade.values()) {
+			if(cod.equals(x.getCodigo())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Prioridade inválida");
+	}
+	
+}
