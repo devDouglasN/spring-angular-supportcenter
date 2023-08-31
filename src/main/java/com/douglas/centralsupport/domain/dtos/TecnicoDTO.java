b1c05f931bcb4703d9126f4331a12ed47aa7f91b
Douglas <douglas_timaofiel@hotmail.com>
2023-08-31 16:28:41 -0300

    Validações durante a criação de novo usuário

--- src/main/java/com/douglas/centralsupport/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/douglas/centralsupport/domain/dtos/TecnicoDTO.java
@@ -10,13 +10,23 @@ import com.douglas.centralsupport.domain.Tecnico;
 import com.douglas.centralsupport.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.validation.constraints.NotNull;
+
 public class TecnicoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	protected Integer id;
+	
+	@NotNull(message = "O preenchimento do campo NOME é obrigatório.")
 	protected String nome;
+	
+	@NotNull(message = "O preenchimento do CPF nome é obrigatório.")
 	protected String cpf;
+	
+	@NotNull(message = "O preenchimento do EMAIL nome é obrigatório.")
 	protected String email;
+	
+	@NotNull(message = "O preenchimento do SENHA nome é obrigatório.")
 	protected String senha;
 	protected Set<Integer> perfis = new HashSet<>();
 
